205f6c8366239c5fc95ec9b5ea3fe9e4eb3fad82
RaianNolaco <raiannolaco@gmail.com>
2023-10-31 22:17:17 -0300

    Estrutura

---
+++ src/main/java/br/com/procurarte/procurarte/ProcurarteApplication.java
@@ -0,0 +1,13 @@
+package br.com.procurarte.procurarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurarteApplication.class, args);
+	}
+
+}
